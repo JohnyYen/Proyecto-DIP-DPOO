e3e06f54ec0a0a733fe0bd4a3584e697f6675003
JohnyYen <126342433+JohnyYen@users.noreply.github.com>
2023-11-16 16:24:02 -0500

    Probando

--- ProyectoDIP/src/ComponentesVisuales/Pruebas.java
+++ ProyectoDIP/src/ComponentesVisuales/Pruebas.java
@@ -6,11 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-<<<<<<< HEAD
 import Util.Corazon;
 import Personajes.Cleopatra;
-=======
->>>>>>> rafael
 import Personajes.Heroe;
 
 public class Pruebas extends JFrame {
@@ -38,36 +35,22 @@ public class Pruebas extends JFrame {
 	 */
 	public Pruebas() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-<<<<<<< HEAD
-		setBounds(100, 100, 450, 300);
-		
-		BarraMenu barraMenu = new BarraMenu();
-		setJMenuBar(barraMenu);
-=======
 		setBounds(100, 100, 610, 464);
->>>>>>> rafael
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-<<<<<<< HEAD
-		CartaVisual cartaVisual = new CartaVisual();
-		cartaVisual.setBounds(104, 71, 287, 146);
-		contentPane.add(cartaVisual);
-		cartaVisual.voltearCarta();
-		
-		Heroe heroe = new Heroe();
-		heroe.setBounds(22, 32, 96, 78);
-		contentPane.add(heroe);
-		
-=======
 		
 		BotonExtendidoNivel2 btnxtndnvl2Hola = new BotonExtendidoNivel2();
 		btnxtndnvl2Hola.setText("Hola");
 		btnxtndnvl2Hola.setBounds(33, 97, 463, 76);
 		contentPane.add(btnxtndnvl2Hola);
->>>>>>> rafael
+		
+		BotonExtendido btnxtndHola = new BotonExtendido();
+		btnxtndHola.setText("Hola");
+		btnxtndHola.setBounds(98, 35, 284, 51);
+		contentPane.add(btnxtndHola);
 		
 		
 		
