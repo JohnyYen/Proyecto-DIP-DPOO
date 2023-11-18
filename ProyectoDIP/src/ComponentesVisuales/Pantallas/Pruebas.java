b371ff1755709d31c027bb4dc03168b0fce2e6be
Rafael Rodriguez <rafaelrdgz2003@gmail.com>
2023-11-18 11:01:08 +0100

    arreglado error al compilar la clase validacion

--- ProyectoDIP/src/ComponentesVisuales/Pantallas/Pruebas.java
+++ ProyectoDIP/src/ComponentesVisuales/Pantallas/Pruebas.java
@@ -1,42 +1,27 @@
 package ComponentesVisuales.Pantallas;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import ComponentesVisuales.Componentes.CuadroDialogo;
+import ComponentesVisuales.Componentes.LabelPreguntas;
 import ComponentesVisuales.Componentes.TextPanel;
 import ComponentesVisuales.Componentes.BotonExtendido;
 
 import javax.swing.JTextArea;
 
-<<<<<<< HEAD
-import Util.Carta;
-
-=======
->>>>>>> rafael
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 import ComponentesVisuales.Componentes.BotonExtendidoNivel2;
-<<<<<<< HEAD
-import ComponentesVisuales.Componentes.CartaVisual;
+import ComponentesVisuales.Niveles.SegundoNivel;
+
 import javax.swing.JProgressBar;
+
 import Personajes.Dullahan;
 import Personajes.Cleopatra;
-=======
-import ComponentesVisuales.Componentes.LabelRespuestasNivelDos;
-import ComponentesVisuales.Niveles.PrimerNivel;
-
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
->>>>>>> rafael
 
 public class Pruebas extends JFrame {
 
@@ -76,12 +61,11 @@ public class Pruebas extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		BotonExtendidoNivel2 btnxtndnvl2Holaaaaaaa = new BotonExtendidoNivel2();
+		/*BotonExtendidoNivel2 btnxtndnvl2Holaaaaaaa = new BotonExtendidoNivel2();
 		btnxtndnvl2Holaaaaaaa.setText("<html>Un bucle \"for\" siempre ejecuta un numero fijo de iteraciones, mientras que un bucle \"while\" depende de una condicion.");
 		btnxtndnvl2Holaaaaaaa.setBounds(184, 224, 643, 113);
 		contentPane.add(btnxtndnvl2Holaaaaaaa);
 		
-<<<<<<< HEAD
 		btnxtndTocame = new BotonExtendido();
 		btnxtndTocame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -94,19 +78,16 @@ public class Pruebas extends JFrame {
 		btnxtndTocame.setText("Tocame");
 		btnxtndTocame.setBounds(222, 150, 210, 28);
 		contentPane.add(btnxtndTocame);
-=======
 		BotonExtendidoNivel2 botonExtendidoNivel2 = new BotonExtendidoNivel2();
 		botonExtendidoNivel2.setText("holaaaaaaa");
 		botonExtendidoNivel2.setBounds(184, 363, 679, 102);
 		contentPane.add(botonExtendidoNivel2);
->>>>>>> rafael
 		
 		BotonExtendidoNivel2 botonExtendidoNivel2_1 = new BotonExtendidoNivel2();
 		botonExtendidoNivel2_1.setText("holaaaaaaa");
 		botonExtendidoNivel2_1.setBounds(168, 504, 679, 102);
 		contentPane.add(botonExtendidoNivel2_1);
 		
-<<<<<<< HEAD
 		
 		
 		JProgressBar progressBar = new JProgressBar();
@@ -120,9 +101,14 @@ public class Pruebas extends JFrame {
 		
 		Cleopatra cleopatra = new Cleopatra();
 		cleopatra.setBounds(322, 215, 110, 125);
-		contentPane.add(cleopatra);
-=======
->>>>>>> rafael
+		contentPane.add(cleopatra);*/
+		
+		LabelPreguntas lb = new LabelPreguntas(0,0,200,100);
+		lb.setBounds(0, 0, 367, 172);
+		contentPane.add(lb);
+		lb.setText("Holaaaa");
+		
+		
 		
 		
 		
--- ProyectoDIP/src/Logica/Validacion.java
+++ ProyectoDIP/src/Logica/Validacion.java
@@ -2,9 +2,9 @@ package Logica;
 
 public class Validacion {
 
-	public Validacion(){
+	/*public Validacion(){
 		
-	}
+	}*/
 	
 	public boolean esValidaContrasenia(char[] password){
 		int cantNumeros = 0,cantLetras = 0, cantMayusculas = 0;
