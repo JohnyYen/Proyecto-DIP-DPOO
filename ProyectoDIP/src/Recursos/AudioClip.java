518efa44fd039993ee698573331e7f7cee3ff1ad
Rafael Rodriguez <rafaelrdgz2003@gmail.com>
2023-12-19 19:02:28 -0500

    corregidos problemas con el audio

--- ProyectoDIP/src/Recursos/AudioClip.java
+++ ProyectoDIP/src/Recursos/AudioClip.java
@@ -6,22 +6,17 @@ import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class AudioClip extends Application {
-	
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        String audioFile = "TemaPrincipal.mp3"; // Ubicación del archivo de audio
-
-        // Crear un objeto Media con el archivo de audio
-        Media media = new Media(getClass().getResource(audioFile).toString());
-
-        // Crear un reproductor de audio
+        String archivoAudio = "TemaPrincipal.mp3";
+        
+        Media media = new Media(getClass().getResource(archivoAudio).toExternalForm());
         MediaPlayer mediaPlayer = new MediaPlayer(media);
-
-        // Reproducir el audio
+        
         mediaPlayer.play();
     }
 }
